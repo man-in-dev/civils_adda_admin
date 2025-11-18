0f804deb0b62f3493be6079f617486df55bb7df5
man-in-dev <man.in.dev.16@gmail.com>
2025-11-18 15:04:00 +0530

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/civils_admin',
-  assetPrefix: '/civils_admin',
+  output: "standalone",
+  basePath: "/civils_admin",
+  assetPrefix: "/civils_admin/",
 };
 
 export default nextConfig;
