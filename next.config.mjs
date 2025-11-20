7f9df21beb1f71dda7319ec1da2862258b5191ef
man-in-dev <man.in.dev.16@gmail.com>
2025-11-20 13:23:38 +0530

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  basePath: "/civils_admin",
-  assetPrefix: "/civils_admin/",
+  // output: "standalone",
+  // basePath: "/civils_admin",
+  // assetPrefix: "/civils_admin/",
 };
 
 export default nextConfig;
